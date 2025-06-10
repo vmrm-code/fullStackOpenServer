ee190284f1fca13f8a1f80557b5d89b8681ba667
vmrm-code <victorvmrm9761@gmail.com>
2025-06-10 19:11:51 +0200

    React app added to the server.

--- part3/phonebook_server/index.js
+++ part3/phonebook_server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (request) => {
   return request.method === 'POST' ? JSON.stringify(request.body) : ''
